ad2fc3d5ecec1d2df707b3db065b32c4973ab6ee
guilec06 <guillaume.lecocq@epitech.eu>
2026-01-12 17:01:17 +0100

    Add IOWrapper class for managing asynchronous I/O operations

---
+++ include/IOWrapper.hpp
@@ -0,0 +1,12 @@
+#ifndef IOWRAPPER_HPP_
+    #define IOWRAPPER_HPP_
+
+#include "Async_IOInterface.hpp"
+
+class IOWrapper {
+    public:
+        IOWrapper() {}
+        ~IOWrapper() = default;
+};
+
+#endif
