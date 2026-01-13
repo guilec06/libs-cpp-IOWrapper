25d1440e2fdde8c38cda60ca53dde23388ce59e6
guilec06 <guillaume.lecocq@epitech.eu>
2026-01-13 18:21:49 +0100

    feat(IOWrapper): add FileIOWrapper and SocketIOWrapper classes
    Add forward declarations for IOWrapper subclasses.
    Introduce FileIOWrapper with read/write methods for file I/O.
    Introduce SocketIOWrapper with send/recv methods for network I/O.
    Update open() factory method to return FileIOWrapper.

--- include/IOWrapper.hpp
+++ include/IOWrapper.hpp
@@ -9,12 +9,26 @@ enum io_mode_t {
     RW
 };
 
+class IOWrapper;
+class FileIOWrapper;
+class SocketIOWrapper;
+
 class IOWrapper {
     public:
-        static IOWrapper open(std::string &path,
+        static FileIOWrapper open(std::string &path,
             bool create=true,
             io_mode_t mode=RW,
             mode_t permissions=0644);
+};
+
+class FileIOWrapper : public IOWrapper {
+    int write(std::string &buffer, int n = -1);
+    int read(std::string &buffer, int n = -1);
+};
+
+class SocketIOWrapper : public IOWrapper {
+    int send(std::string &buffer, int n = -1);  
+    int recv(std::string &buffer, int n = -1);
 };
 
 #endif
